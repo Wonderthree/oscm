7ad8a7fce2d50c80e9917dba6f80105821170925
Wonderthree <mingao5-c@my.cityu.edu.hk>
2016-07-14 17:13:55 +0800

    added TODOs for Simulator

--- oscm/src/Main.java
+++ oscm/src/Main.java
@@ -4,9 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		
-		Demand demand = new ConstantDemand(10);
+		//Demand demand = new ConstantDemand(10);
+		Demand demand=new UniformDemand(20,0);
 		//Policy policy = new ConstantPolicy(5);
-		Policy policy = new ReorderPointPolicy(20, 50);
+		Policy policy = new ReorderPointPolicy(10, 50);
 		int initialInventory = 20;
 		Simulator simulator = new Simulator(initialInventory,
 				demand, policy, 10);
--- oscm/src/Simulator.java
+++ oscm/src/Simulator.java
@@ -1,8 +1,15 @@
 
 public class Simulator {
 
+	private int cycle = 5;
+    private int leadTime = 2;
+	
 	public Simulator(int initialInventory, 
 			Demand demand, Policy policy, int periods) {
+		// Go to lesson 7.2
+		int[] pipeline = new int[leadTime + 1];
+		// pipeline[k] = shipment to arrive in period t + k
+		
 		// TODO add box size, max inventory level
 		// TODO lead times for shipments
 		// TODO much much later: demand and policy for multiple drugs
@@ -10,8 +17,14 @@ public class Simulator {
 		for (int t = 0; t < periods; ++t) {
 			// i: beginning-of-period inventory
 			System.out.printf("i[%d] = %d, ", t, inv);
-			// Step 1: order new inventory
-			int q = policy.order(inv);
+
+			int q = 0;
+			if (t % cycle == 0) {
+				// Step 1: order new inventory
+				q = policy.order(inv);
+				pipeline[leadTime] = q;
+			}
+
 			// Step 2: inventory arrives (after lead time)
 			inv += q;
 			// q: order quantity
@@ -21,10 +34,16 @@ public class Simulator {
 			// d: demand
 			System.out.printf("d[%d] = %d, ", t, d);
 			inv -= d;
+			
+			// TODO keep inventory positive, set unmet demand
+			
 			// j: end-of-period inventory
 			System.out.printf("j[%d] = %d\n", t, inv);
+			
+			// update the pipeline
+			// TODO
 		}
-		
+
 		// TODO print metrics such as:
 		// average inventory level
 		// unmet demand
@@ -32,5 +51,5 @@ public class Simulator {
 		// number of shipments
 		// etc.
 	}
-	
+
 }
